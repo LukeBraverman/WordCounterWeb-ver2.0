558a23204cfbff8f6c83809b76904dbf26ef08e4
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-08 21:44:10 +0100

    Controllers added and database configed

---
+++ src/main/java/com/pollinate/wordcounter/wordcounter/WordControllers/AddWordController.java
@@ -0,0 +1,22 @@
+package com.pollinate.wordcounter.wordcounter.WordControllers;
+
+import com.pollinate.wordcounter.wordcounter.WordObject.WordService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(path = "/WordCounter")
+public class AddWordController {
+    private  WordService wordService;
+
+
+    @PostMapping(path = "/AddWord")
+    public ModelAndView addWordToDatabse(@RequestParam(value = "wordToAdd") String wordToAdd) {
+        wordService.addWord(wordToAdd);
+
+        //ADD VIEWS
+    }
+}
