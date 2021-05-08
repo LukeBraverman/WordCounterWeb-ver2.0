26608a14d69fd749e1af2360076ba1dd89f125a6
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-08 20:58:16 +0100

    Creating repo

---
+++ src/test/java/com/pollinate/wordcounter/wordcounter/WordcounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollinate.wordcounter.wordcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
